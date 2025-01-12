690dc55daf641a1c72e233c357c69dfe91ea8889
Arthur Djiomou <arthurdjiomou711@gmail.com>
2025-01-12 21:34:51 +0100

    visual bug fix
    fixed the calculator expanding when the result has too many decimals

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const display = document.querySelector('#display')
 const buttons = document.querySelectorAll('button')
 
+var bracketOpen = false
+
 buttons.forEach((item)=>{
     item.onclick = ()=>{
 
@@ -12,10 +14,22 @@ buttons.forEach((item)=>{
             display.innerText = string.substr(0, string.length-1)
         }
         else if (display.innerText != '' && item.id == 'equal'){
-            display.innerText = eval(display.innerText)
+            var answer = eval(display.innerText)
+            let string = String(answer)
+            display.innerText = string.substr(0, 13)
         }
         else if (display.innerText == '' && item.id == 'equal'){
             display.innerText == ''
+        }
+        else if (item.id == 'brackets'){
+            if (bracketOpen == false){
+                display.innerText += '('
+                bracketOpen = true
+            }
+            else {
+                display.innerText += ')'
+                bracketOpen = false
+            }
         }
         else{
             display.innerText += item.id
